0f71a922d454d6a33f2dd63a33c6a60a7ab61b36
Stars1999 <kerwinlohzy@gmail.com>
2025-02-20 21:50:01 +0800

    Basic Webpage

---
+++ ICT2106WebApp/Pages/Logger.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ICT2106WebApp.Pages
+{
+    public class LoggerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

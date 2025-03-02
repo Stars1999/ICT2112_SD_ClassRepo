c8d7c8d966aecf214b9e02124814e8650af5f1e3
Huang ShiLong <phantasmlied@gmail.com>
2025-03-02 14:06:05 +0800

    setup env

---
+++ ICT2106WebApp/Controllers/YourNewController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICT2106WebApp.Controllers
+{
+    public class YourNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Add your new actions/methods here
+    }
+}
---
+++ ICT2106WebApp/Models/YourModel.cs
@@ -0,0 +1,10 @@
+namespace ICT2106WebApp.Models
+{
+    public class YourModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Add your new properties/methods here
+    }
+}

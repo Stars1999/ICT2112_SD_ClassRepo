45786ccbc9804d82d24e1aa201508a0d72b2d891
Huang ShiLong <phantasmlied@gmail.com>
2025-03-12 10:45:50 +0800

    early push for directory setup

---
+++ ICT2106WebApp/mod2grp6/Layout/LayoutManager.cs
@@ -0,0 +1,52 @@
+namespace ICT2106WebApp.mod2grp6.Format
+{
+    public class LayoutManager
+    {
+        private Layout content;
+
+        public bool StartLayoutFormatting(Layout content)
+        {
+            return false;
+        }
+
+        public bool FormatHeaders()
+        {
+            return false;
+        }
+
+        public bool FormatFooters()
+        {
+            return false;
+        }
+
+        public bool FormatMargins()
+        {
+            return false;
+        }
+
+        public bool FormatOrientation()
+        {
+            return false;
+        }
+
+        public bool FormatPageSize()
+        {
+            return false;
+        }
+
+        public bool FormatColumnNum()
+        {
+            return false;
+        }
+
+        public bool FormatColumnSpacing()
+        {
+            return false;
+        }
+
+/*         public List<AbstractNode> ApplyLayoutFormatting()
+        {
+            return null;
+        } */
+    }
+}

b8a09020265966c756ce850b50db416951f6a7f0
Huang ShiLong <phantasmlied@gmail.com>
2025-03-14 15:47:17 +0800

    added cl classes with func prototype

---
+++ ICT2106WebApp/mod2grp6/DocumentManager.cs
@@ -0,0 +1,7 @@
+namespace ICT2106WebApp.mod2grp6
+{
+    
+    public class DocumentManager{
+
+    }
+}
---
+++ ICT2106WebApp/mod2grp6/FormatConversionManager.cs
@@ -0,0 +1,31 @@
+namespace ICT2106WebApplication.mod2grp6 {
+    using ICT2106WebApp.mod2grp6.Format;
+    using ICT2106WebApp.mod2grp6.Layout;
+    using ICT2106WebApp.mod2grp6.Text;
+    using ICT2106WebApp.Utilities;
+
+    public class FormatConversionManager {
+
+        private List<AbstractNode> content;
+
+        public bool convertFormat(List<AbstractNode> content){
+            return false;  //placeholder
+        }
+
+
+        public bool convertLayout(List<AbstractNode> content)
+        {
+            return false;  //placeholder
+        }
+
+
+        public bool convertText(List<AbstractNode> content)
+        {
+            return false;  //placeholder
+        }
+
+        public List<AbstractNode> getContent(){
+            return content;
+        }
+    }
+}

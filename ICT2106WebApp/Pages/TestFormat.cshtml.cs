c7184e318b0af940812f4f33069910f377cccd66
bernice1606 <bernicegoo16@gmail.com>
2025-03-20 14:31:35 +0800

    edits

--- ICT2106WebApp/Pages/TestFormat.cshtml.cs
+++ ICT2106WebApp/Pages/TestFormat.cshtml.cs
@@ -1,37 +1,9 @@
-// using Microsoft.AspNetCore.Mvc.RazorPages;
-// using ICT2106WebApp.mod2grp6.Format;
-// using ICT2106WebApp.Utilities;
-// using System.Collections.Generic;
-
-// namespace ICT2106WebApp.Pages
-// {
-//    public class TestFormatModel : PageModel
-//    {
-//        public bool? Result { get; set; }
-//        public List<AbstractNode> Content { get; set; }
-
-//        public void OnPost()
-//        {
-//            // Create example nodes
-//            Content = new List<AbstractNode>
-//            {
-//                new SimpleNode(1, "h1", "Heading 1", new List<Dictionary<string, object>> { new Dictionary<string, object> { { "Bold", true } } }),
-//                new SimpleNode(2, "h2", "Heading 2", new List<Dictionary<string, object>> { new Dictionary<string, object> { { "Italic", true } } }),
-//                new CompositeNode(3, "h3", "Heading 3", new List<Dictionary<string, object>> { new Dictionary<string, object> { { "Bold", true }, { "FontColor", "434343" } } })
-//            };
-
-//            // Create and test FormatManager
-//            var formatManager = new FormatManager();
-//            formatManager.StartFormatting(Content);
-//            Result = formatManager.FormatHeadings();
-//        }
-//    }
-// }
-
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ICT2106WebApp.mod2grp6.Layout;
+using ICT2106WebApp.mod2grp6.Format;
 using ICT2106WebApp.Utilities;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ICT2106WebApp.Pages
 {
@@ -39,10 +11,19 @@ namespace ICT2106WebApp.Pages
     {
         public bool? Result { get; set; }
         public List<AbstractNode> Content { get; set; }
+        public string TestType { get; set; } = "None";
 
-        public void OnPost()
+        public void OnGet()
         {
-            // Create example nodes based on output.json examples
+            // Initialize empty content
+            Content = new List<AbstractNode>();
+        }
+
+        public IActionResult OnPostTestLayout()
+        {
+            TestType = "Layout";
+            
+            // Create example nodes based on output.json examples - keep original code
             Content = new List<AbstractNode>
             {
                 new SimpleNode(1, "header", "Header from output.json", 
@@ -71,13 +52,13 @@ namespace ICT2106WebApp.Pages
                     })
             };
 
-            // Create and test LayoutManager with the List<AbstractNode>
+            // Create and test LayoutManager with the List<AbstractNode> - keep original code
             var layoutManager = new LayoutManager();
             bool success = layoutManager.StartLayoutFormatting(Content);
 
             if (success)
             {
-                // Apply layout formatting
+                // Apply layout formatting - keep original code
                 Content = layoutManager.ApplyLayoutFormatting();
                 Result = true;
             }
@@ -86,6 +67,40 @@ namespace ICT2106WebApp.Pages
                 Result = false;
                 Content = new List<AbstractNode>();
             }
+            
+            return Page();
+        }
+
+        public IActionResult OnPostTestFormat()
+        {
+            TestType = "Format";
+            
+            // Create example nodes for FormatManager testing
+            Content = new List<AbstractNode>
+            {
+                new SimpleNode(1, "h1", "Heading 1", 
+                    new List<Dictionary<string, object>> { 
+                        new Dictionary<string, object> { { "Bold", true } } 
+                    }),
+                new SimpleNode(2, "h2", "Heading 2", 
+                    new List<Dictionary<string, object>> { 
+                        new Dictionary<string, object> { { "Italic", true } } 
+                    }),
+                new SimpleNode(3, "h3", "Heading 3", 
+                    new List<Dictionary<string, object>> { 
+                        new Dictionary<string, object> { 
+                            { "Bold", true }, 
+                            { "FontColor", "434343" } 
+                        } 
+                    })
+            };
+
+            // Create and test FormatManager
+            var formatManager = new FormatManager();
+            formatManager.StartFormatting(Content);
+            Result = formatManager.FormatHeadings();
+            
+            return Page();
         }
     }
 }

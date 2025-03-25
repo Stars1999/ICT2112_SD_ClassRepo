e66a4b113cf83963266b9e6427464cc031c5ce37
Joel Kong <joelkong2001@gmail.com>
2025-03-25 11:40:16 +0800

    clean

--- ICT2106WebApp/mod1grp4/validation/iTableValidate.cs
+++ ICT2106WebApp/mod1grp4/validation/iTableValidate.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
+
 namespace ICT2106WebApp.mod1grp4
 {
-    // iTableValidate (Hiew Teng - COMPLETED)
     public interface iTableValidate
     {
         public string validateTableLatexOutput(List<AbstractNode> originalNodes, List<Table> processedTables);

fbb6941fff940b70b27cbc8b6cc3e7d1f3e7aacf
leonseehaojun <leonsee1000@yahoo.com.sg>
2025-03-25 22:04:18 +0800

    Updated Route to /understem

--- ICT2106WebApp/Controllers/LatexEditorApplicationController.cs
+++ ICT2106WebApp/Controllers/LatexEditorApplicationController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Text.Json;
 using MongoDB.Driver;
 
-[Route("home")]
+[Route("understem")]
 public class LatexEditorApplicationController : Controller
 {
     private readonly iConversionStatus _conversionStatus;

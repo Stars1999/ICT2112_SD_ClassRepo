52ac4ec4050cb4ed57592c8c2a059e0f8302e5c1
Huang ShiLong <phantasmlied@gmail.com>
2025-03-28 22:42:30 +0800

    removed irrelevant cshtml files

--- ICT2106WebApp/mod2grp6/Template/TemplateDocument.cs
+++ ICT2106WebApp/mod2grp6/Template/TemplateDocument.cs
@@ -1,13 +1,35 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
-using ICT2106WebApp.Utilities; // Add this namespace for AbstractNode
+using ICT2106WebApp.Utilities;
+using MongoDB.Bson.Serialization;
 
 namespace ICT2106WebApp.mod2grp6.Template
 {
     [BsonIgnoreExtraElements]
     public class TemplateDocument
     {
+        static TemplateDocument()
+        {
+            // Register class maps for deserialization
+            if (!BsonClassMap.IsClassMapRegistered(typeof(SimpleNode)))
+            {
+                BsonClassMap.RegisterClassMap<SimpleNode>(cm =>
+                {
+                    cm.AutoMap();
+                    cm.SetIsRootClass(true);
+                });
+            }
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(CompositeNode)))
+            {
+                BsonClassMap.RegisterClassMap<CompositeNode>(cm =>
+                {
+                    cm.AutoMap();
+                });
+            }
+        }
+
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         [BsonElement("_id")]
@@ -20,6 +42,32 @@ namespace ICT2106WebApp.mod2grp6.Template
         public string TemplateName { get; set; }
         
         [BsonElement("Content")]
-        public List<AbstractNode> Content { get; set; }
+        [BsonIgnoreIfNull]
+        public List<SimpleNode> Content { get; set; } = new List<SimpleNode>();
+        
+        // Convert MongoDB document to Template object
+        public Template ToTemplate()
+        {
+            // Cast SimpleNode list to AbstractNode list since SimpleNode inherits from AbstractNode
+            List<AbstractNode> abstractNodes = Content?.Cast<AbstractNode>().ToList() ?? new List<AbstractNode>();
+            return new Template(Id, TemplateName, abstractNodes);
+        }
+        
+        // Create document from Template
+        public static TemplateDocument FromTemplate(Template template)
+        {
+            // Cast AbstractNode list to SimpleNode list (this assumes all nodes are SimpleNodes)
+            List<SimpleNode> simpleNodes = template.GetContent()?
+                .Where(n => n is SimpleNode)
+                .Cast<SimpleNode>()
+                .ToList() ?? new List<SimpleNode>();
+                
+            return new TemplateDocument
+            {
+                Id = template.GetId(),
+                TemplateName = template.GetTemplateName(),
+                Content = simpleNodes
+            };
+        }
     }
 }

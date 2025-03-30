7f593dfa8bd8cfc8ce10e81d62de80ccdaa066eb
Huang ShiLong <phantasmlied@gmail.com>
2025-03-30 17:51:37 +0800

    removed grp2 stuff

--- ICT2106WebApp/mod2grp2/IAdvancedConversion.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using ICT2106WebApp.Utilities;
-
-public interface IAdvancedConversion
-{
-    List<AbstractNode> getContent();
-}
--- ICT2106WebApp/mod2grp2/IProcessor.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using ICT2106WebApp.Utilities;
-
-public interface IProcessor
-{
-    List<AbstractNode> convertContent(ContentType type);
-}

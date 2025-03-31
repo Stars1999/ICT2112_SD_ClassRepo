999893bb28dc88b9c96b03487b51a7ad29f3455a
andrea <20023855@myrp.edu.sg>
2025-03-31 18:02:36 +0800

    Added styling and change cell styling entity

--- ICT2106WebApp/mod1grp4/preprocessing (Joel)/CellStyling.cs
+++ ICT2106WebApp/mod1grp4/preprocessing (Joel)/CellStyling.cs
@@ -1,6 +1,6 @@
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace ICT2106WebApp.mod1grp4 {    
+namespace ICT2106WebApp.mod1grp4 {
     // CellStyling (Joel - COMPLETED)
     public class CellStyling
     {
@@ -26,19 +26,77 @@ namespace ICT2106WebApp.mod1grp4 {
         [BsonElement("horizontalalignment")]
         public string horizontalalignment { get; set; }
 
-        [BsonElement("border")]
-        public Dictionary<string, string> border { get; set; }
 
-        [BsonElement("size")]
-        public Dictionary<string, string> size { get; set; }
+        [BsonElement("topstyle")]
+        public string topstyle { get; set; }
+
+        [BsonElement("bottomstyle")]
+        public string bottomstyle { get; set; }
+
+        [BsonElement("leftstyle")]
+        public string leftstyle { get; set; }
+
+        [BsonElement("rightstyle")]
+        public string rightstyle { get; set; }
+
+        [BsonElement("topwidth")]
+        public string topwidth { get; set; }
+
+        [BsonElement("bottomwidth")]
+        public string bottomwidth { get; set; }
+
+        [BsonElement("leftwidth")]
+        public string leftwidth { get; set; }
+
+        [BsonElement("rightwidth")]
+        public string rightwidth { get; set; }
+
+        [BsonElement("topcolor")]
+        public string topcolor { get; set; }
+
+        [BsonElement("bottomcolor")]
+        public string bottomcolor { get; set; }
+
+        [BsonElement("leftcolor")]
+        public string leftcolor { get; set; }
+
+        [BsonElement("rightcolor")]
+        public string rightcolor { get; set; }
+
+        [BsonElement("bordertopstyle")]
+        public string bordertopstyle { get; set; }
+
+        [BsonElement("borderbottomstyle")]
+        public string borderbottomstyle { get; set; }
+
+        [BsonElement("borderleftstyle")]
+        public string borderleftstyle { get; set; }
+
+        [BsonElement("borderrightstyle")]
+        public string borderrightstyle { get; set; }
+
+        [BsonElement("bordertopwidth")]
+        public string bordertopwidth { get; set; }
+
+        [BsonElement("borderbottomwidth")]
+        public string borderbottomwidth { get; set; }
+
+
+
+        [BsonElement("cellWidth")]
+        public string cellWidth { get; set; }
+
+        [BsonElement("rowHeight")]
+        public string rowHeight { get; set; }
 
         [BsonElement("backgroundcolor")]
         public string backgroundcolor { get; set; }
 
         public CellStyling()
         {
-            border = new Dictionary<string, string>();
-            size = new Dictionary<string, string>();
+            // border = new Dictionary<string, string>();
+            // size = new Dictionary<string, string>();
         }
+
     }
 }

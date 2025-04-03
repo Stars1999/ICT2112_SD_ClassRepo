199d07b08dd407d3452cd2474d82c1de5c6a5a1f
2202210 <2202210@sit.singaporetech.edu.sg>
2025-04-03 13:50:27 +0800

    remove some lines

--- ICT2106WebApp/Program.cs
+++ ICT2106WebApp/Program.cs
@@ -263,94 +263,63 @@ public static class DocumentProcessor
 			// GROUP 4 STUFF
 			// Step 1: Get abstract nodes of table from group 3
 			INodeTraverser traverser = new NodeTraverser(rootnodehere);
-			/*
-			
-						List<AbstractNode> tableAbstractNodes = traverser.TraverseNode("tables");
-			
-						// Step 2: Convert abstract node to custom table entity (JOEL)
-						var tableOrganiser = new TableOrganiserManager();
-						List<ICT2106WebApp.mod1grp4.Table> tablesFromNode = tableOrganiser.organiseTables(
-							tableAbstractNodes
-						);
-			
-						// Step 3: Preprocess tables (setup observer, recover backup tables if exist, fix table integrity) (JOEL)
-						var rowTabularGateway_RDG = new RowTabularGateway_RDG(database);
-						var tablePreprocessingManager = new TablePreprocessingManager();
-						tablePreprocessingManager.attach(rowTabularGateway_RDG);
-						var tables = await tablePreprocessingManager.recoverBackupTablesIfExist(tablesFromNode);
-						List<ICT2106WebApp.mod1grp4.Table> cleanedTables =
-							await tablePreprocessingManager.fixTableIntegrity(tables);
-			
-						// Step 4: Convert tables to LaTeX (ANDREA)
-						var latexConversionManager = new TableLatexConversionManager();
-						latexConversionManager.attach(rowTabularGateway_RDG);
-			
-						// NORMAL FLOW (this will prove for Andrea where she inserts the content to overleaf and jonathan for styling of table)
-						List<ICT2106WebApp.mod1grp4.Table> processedTables =
-							await latexConversionManager.convertToLatexAsync(cleanedTables);
-			
-						// JOEL CRASH RECOVERY FLOW (we will convert 2 tables then stop the program, this will prove for Joel run crash flow first then normal again)
-						// List<ICT2106WebApp.mod1grp4.Table> processedTables = await latexConversionManager.convertToLatexWithLimitAsync(cleanedTables, 2);
-						// Environment.Exit(0);
-			
-						// HIEW TENG VALIDATION CHECK FLOW (we will omit out some stuff in the latex conversion, will prove for hiew teng where validation is wrong)
-						// List<ICT2106WebApp.mod1grp4.Table> processedTables = await latexConversionManager.convertToLatexStyleFailAsync(cleanedTables);
-			
-						// Step 5: Post-processing (validation of latex, logging of validation status, convert processed tables to nodes to send over) (HIEW TENG AND SITI)
-						var tableValidationManager = new TableValidationManager();
-						var validationStatus = tableValidationManager.validateTableLatexOutput(
-							tableAbstractNodes,
-							processedTables
-						);
-			
-						var processedTableManager = new ProcessedTableManager();
-						processedTableManager.attach(rowTabularGateway_RDG);
-						processedTableManager.logProcessingStatus(validationStatus);
-						await processedTableManager.slotProcessedTableToTree(cleanedTables, tableAbstractNodes);
-			
-						// Will prove for Siti as we traverse the nodes again after updating
-						// List<AbstractNode> endingTableAbstractNodes = traverser.TraverseNode("tables");
-			*/
+
+			List<AbstractNode> tableAbstractNodes = traverser.TraverseNode("tables");
+
+			// Step 2: Convert abstract node to custom table entity (JOEL)
+			var tableOrganiser = new TableOrganiserManager();
+			List<ICT2106WebApp.mod1grp4.Table> tablesFromNode = tableOrganiser.organiseTables(
+				tableAbstractNodes
+			);
+
+			// Step 3: Preprocess tables (setup observer, recover backup tables if exist, fix table integrity) (JOEL)
+			var rowTabularGateway_RDG = new RowTabularGateway_RDG(database);
+			var tablePreprocessingManager = new TablePreprocessingManager();
+			tablePreprocessingManager.attach(rowTabularGateway_RDG);
+			var tables = await tablePreprocessingManager.recoverBackupTablesIfExist(tablesFromNode);
+			List<ICT2106WebApp.mod1grp4.Table> cleanedTables =
+				await tablePreprocessingManager.fixTableIntegrity(tables);
+
+			// Step 4: Convert tables to LaTeX (ANDREA)
+			var latexConversionManager = new TableLatexConversionManager();
+			latexConversionManager.attach(rowTabularGateway_RDG);
+
+			// NORMAL FLOW (this will prove for Andrea where she inserts the content to overleaf and jonathan for styling of table)
+			List<ICT2106WebApp.mod1grp4.Table> processedTables =
+				await latexConversionManager.convertToLatexAsync(cleanedTables);
+
+			// JOEL CRASH RECOVERY FLOW (we will convert 2 tables then stop the program, this will prove for Joel run crash flow first then normal again)
+			// List<ICT2106WebApp.mod1grp4.Table> processedTables = await latexConversionManager.convertToLatexWithLimitAsync(cleanedTables, 2);
+			// Environment.Exit(0);
+
+			// HIEW TENG VALIDATION CHECK FLOW (we will omit out some stuff in the latex conversion, will prove for hiew teng where validation is wrong)
+			// List<ICT2106WebApp.mod1grp4.Table> processedTables = await latexConversionManager.convertToLatexStyleFailAsync(cleanedTables);
+
+			// Step 5: Post-processing (validation of latex, logging of validation status, convert processed tables to nodes to send over) (HIEW TENG AND SITI)
+			var tableValidationManager = new TableValidationManager();
+			var validationStatus = tableValidationManager.validateTableLatexOutput(
+				tableAbstractNodes,
+				processedTables
+			);
+
+			var processedTableManager = new ProcessedTableManager();
+			processedTableManager.attach(rowTabularGateway_RDG);
+			processedTableManager.logProcessingStatus(validationStatus);
+			await processedTableManager.slotProcessedTableToTree(cleanedTables, tableAbstractNodes);
+
+			// Will prove for Siti as we traverse the nodes again after updating
+			// List<AbstractNode> endingTableAbstractNodes = traverser.TraverseNode("tables");
+
 			// Save modified latex tree back to MongoDB (query)
 			await traverser.UpdateLatexDocument(rootnodehere);
 
 			// // Retrieve the non-modified tree from MongoDB (for demo query)
 			ICompletedLatex completedLatex = new CompletedLatex();
-			// AbstractNode originalRootNode = await completedLatex.RetrieveTree();
-			// CompositeNode originalMongo = null; // declare outside so it can be used outside of the if statement
+			// await completedLatex.RetrieveTree();
 
-			// if (originalRootNode is CompositeNode originalNode) // Use pattern matching
-			// {
-			// 	Console.WriteLine("Latex Tree retrieved!");
-			// 	originalMongo = originalNode; // Assign to compNode
-			// }
-			// else
-			// {
-			// 	Console.WriteLine("Latex Tree not retrieved!");
-			// }
-			// if (originalMongo != null)
-			// {
-			// 	treeProcessor.PrintTree(originalMongo,0);
-			// }
 
-			// // Retrieve the Latex tree from MongoDB (for demo query)
-			// AbstractNode latexRootNode = await completedLatex.RetrieveLatexTree();
-			// CompositeNode latexMongo = null; // declare outside so it can be used outside of the if statement
-
-			// if (latexRootNode is CompositeNode latexNode) // Use pattern matching
-			// {
-			// 	Console.WriteLine("Latex Tree retrieved!");
-			// 	latexMongo = latexNode; // Assign to compNode
-			// }
-			// else
-			// {
-			// 	Console.WriteLine("Latex Tree not retrieved!");
-			// }
-			// if (latexMongo != null)
-			// {
-			// 	treeProcessor.PrintTree(latexMongo,0);
-			// }
-
+			// Retrieve the Latex tree from MongoDB (for demo query)
+			await completedLatex.RetrieveLatexTree();
 
 			// foreach (var tableNode in tableAbstractNodes)
 			// {
